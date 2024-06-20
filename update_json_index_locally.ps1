1dfa86eeabb8524cf43bc695436df57917967632
Nebiz <24771276+Nebiz@users.noreply.github.com>
2024-06-19 20:51:17 -0400

    remove ps script

--- update_json_index_locally.ps1
+++
@@ -1,5 +0,0 @@
-# Get a list of all JSON files in the folder, excluding those with "bug" in the filename
-$jsonFiles = Get-ChildItem -Path "json" -Filter *.json | Where-Object { ($_.Name -notmatch "localTest") -and ($_.Name -ne "index.json") } | Select-Object -Expand Name
-
-# Convert the list of JSON files to a JSON object || Write the JSON object to the output file
-$jsonFiles | ConvertTo-Json | Out-File -FilePath "index.json"
